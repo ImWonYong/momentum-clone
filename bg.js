cccb5f3073fe85a11ee6d57207f1961d3c0c1973
ImWonYong <ssassb123@daum.net>
2020-06-15 03:48:51 +0900

    정적 페이지에서도 이미지 찾을 수 있도록 해보기

--- bg.js
+++ bg.js
@@ -4,7 +4,9 @@ const IMG_NUMBER = 5;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `./images/${imgNumber + 1}.jpg`;
+  image.src = `https://raw.githubusercontent.com/ImWonYong/momentum-clone/master/Images/${
+    imgNumber + 1
+  }.jpg`;
   body.appendChild(image);
   image.classList.add("bgImage");
   // image.addEventListener('loadend', handleImgLoad) API로 받아올때 loadend를 사용할 수 있을 것
